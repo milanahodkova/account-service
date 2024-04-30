d1ed9618fac9550bcff389f0c38258f1e34a3b34
milanahodkova <92371808+milanahodkova@users.noreply.github.com>
2024-04-30 06:38:30 +0300

    Modify test

--- service/src/test/java/org/project/AccountServiceApplicationTest.java
+++ service/src/test/java/org/project/AccountServiceApplicationTest.java
@@ -1,7 +1,13 @@
 package org.project;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class AccountServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
 
 }
